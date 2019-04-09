5ee7dcb8115a043240839275cf74501b79bf3bc4
Alena Semushenko <asemushenko@wallarm.com>
2019-04-10 04:08:36 +0700

    fix again

---
+++ assets/index.js
@@ -0,0 +1,10 @@
+$('img').each(function(index, img) {
+  let currentImg = $(img);
+  currentImg.viewer({
+    navbar: false,
+    toolbar: false,
+    fullscreen: false,
+    loop: false,
+    rotatable: false,
+  });
+});
---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports = {
+  book: {
+    assets: './assets',
+    js: [
+      'index.js',
+      'jquery.min.js',
+      'jquery-viewer.min.js',
+      'viewer.min.js'
+    ],
+    css: [
+      'viewer.min.css'
+    ],
+  },
+  hooks: {},
+};
