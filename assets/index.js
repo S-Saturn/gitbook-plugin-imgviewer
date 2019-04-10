0a22a83da8fe6d317265bcf997cd62d94b530098
Alena Semushenko <asemushenko@wallarm.com>
2019-04-10 22:22:21 +0700

    Add white background for images

--- assets/index.js
+++ assets/index.js
@@ -2,6 +2,7 @@ require(['gitbook'], function(gitbook) {
   gitbook.events.bind('page.change', function() {
     $('img').each(function(index, img) {
       let currentImg = $(img);
+      currentImg.css('background-color', 'white');
       currentImg.viewer({
         navbar: false,
         toolbar: false,
