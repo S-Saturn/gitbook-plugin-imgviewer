48f635106cf459b49fe65ea90e503bb912fb3fff
Alena Semushenko <asemushenko@wallarm.com>
2019-04-10 21:48:25 +0700

    test again

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,24 @@
-$('img').each(function(index, img) {
-  let currentImg = $(img);
-  currentImg.viewer({navbar: false, toolbar: false, fullscreen: false, loop: false, rotatable: false,});
+$(function() {
+  // $('img').each(function(index, img) {
+  //   let currentImg = $(img);
+  //   currentImg.viewer({
+  //     navbar: false,
+  //     toolbar: false,
+  //     fullscreen: false,
+  //     loop: false,
+  //     rotatable: false,
+  //   });
+  // });
+  $('.book-body').change(function() {
+    $('img').each(function(index, img) {
+      let currentImg = $(img);
+      currentImg.viewer({
+        navbar: false,
+        toolbar: false,
+        fullscreen: false,
+        loop: false,
+        rotatable: false,
+      });
+    });
+  });
 });
